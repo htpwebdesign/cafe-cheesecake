b32f708ee42566fbfe3fca2ccd7c32bab01ef5f1
Alexander Sheh <alexgjsheh@gmail.com>
2023-03-20 12:51:16 -0700

    Added CPTs and Taxonomies

--- functions.php
+++ functions.php
@@ -182,3 +182,285 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+/**
+* Custom Post Types & Taxonomies
+*/
+
+
+function cafe_cheesecake_register_custom_post_types() {
+	// Register Menu CPT
+	$labels = array(
+	   'name'                  => _x( 'Menu', 'post type general name' ),
+	   'singular_name'         => _x( 'Menu', 'post type singular name'),
+	   'menu_name'             => _x( 'Menu', 'admin menu' ),
+	   'name_admin_bar'        => _x( 'Menu', 'add new on admin bar' ),
+	   'add_new'               => _x( 'Add New', 'menu' ),
+	   'add_new_item'          => __( 'Add New Menu' ),
+	   'new_item'              => __( 'New Menu' ),
+	   'edit_item'             => __( 'Edit Menu' ),
+	   'view_item'             => __( 'View Menu' ),
+	   'all_items'             => __( 'All Menus' ),
+	   'search_items'          => __( 'Search Menus' ),
+	   'parent_item_colon'     => __( 'Parent Menus:' ),
+	   'not_found'             => __( 'No menus found.' ),
+	   'not_found_in_trash'    => __( 'No menus found in Trash.' ),
+	   'archives'              => __( 'Menu Archives'),
+	   'insert_into_item'      => __( 'Insert into menu'),
+	   'uploaded_to_this_item' => __( 'Uploaded to this menu'),
+	   'filter_item_list'      => __( 'Filter menus list'),
+	   'items_list_navigation' => __( 'Menus list navigation'),
+	   'items_list'            => __( 'Menus list'),
+	   'featured_image'        => __( 'Menu featured image'),
+	   'set_featured_image'    => __( 'Set menu featured image'),
+	   'remove_featured_image' => __( 'Remove menu featured image'),
+	   'use_featured_image'    => __( 'Use as featured image'),
+   );
+
+   $args = array(
+	   'labels'             => $labels,
+	   'public'             => true,
+	   'publicly_queryable' => true,
+	   'show_ui'            => true,
+	   'show_in_menu'       => true,
+	   'show_in_nav_menus'  => true,
+	   'show_in_admin_bar'  => true,
+	   'show_in_rest'       => true,
+	   'query_var'          => true,
+	   'rewrite'            => array( 'slug' => 'menu' ),
+	   'capability_type'    => 'post',
+	   'has_archive'        => true,
+	   'hierarchical'       => false,
+	   'menu_position'      => 5,
+	   'menu_icon'          => 'dashicons-archive',
+	   'supports'           => array( 'title', 'thumbnail' ),
+   );
+
+   register_post_type( 'cafe-menu', $args );
+
+	// Register Jobs CPT
+	$labels = array(
+		'name'                  => _x( 'Jobs', 'post type general name' ),
+		'singular_name'         => _x( 'Job', 'post type singular name'),
+		'menu_name'             => _x( 'Jobs', 'admin menu' ),
+		'name_admin_bar'        => _x( 'Job', 'add new on admin bar' ),
+		'add_new'               => _x( 'Add New', 'job' ),
+		'add_new_item'          => __( 'Add New Job' ),
+		'new_item'              => __( 'New Job' ),
+		'edit_item'             => __( 'Edit Job' ),
+		'view_item'             => __( 'View Job' ),
+		'all_items'             => __( 'All Jobs' ),
+		'search_items'          => __( 'Search Jobs' ),
+		'parent_item_colon'     => __( 'Parent Jobs:' ),
+		'not_found'             => __( 'No jobs found.' ),
+		'not_found_in_trash'    => __( 'No jobs found in Trash.' ),
+		'archives'              => __( 'Job Archives'),
+		'insert_into_item'      => __( 'Insert into job'),
+		'uploaded_to_this_item' => __( 'Uploaded to this job'),
+		'filter_item_list'      => __( 'Filter jobs list'),
+		'items_list_navigation' => __( 'Jobs list navigation'),
+		'items_list'            => __( 'Jobs list'),
+		'featured_image'        => __( 'Job featured image'),
+		'set_featured_image'    => __( 'Set job featured image'),
+		'remove_featured_image' => __( 'Remove job featured image'),
+		'use_featured_image'    => __( 'Use as featured image'),
+	);
+ 
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'show_in_nav_menus'  => true,
+		'show_in_admin_bar'  => true,
+		'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'jobs' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'menu_icon'          => 'dashicons-archive',
+		'supports'           => array( 'title', 'editor' ),
+	);
+ 
+	register_post_type( 'cafe-jobs', $args );
+ 
+
+	// Register Catering CPT
+	$labels = array(
+		'name'                  => _x( 'Catering', 'post type general name' ),
+		'singular_name'         => _x( 'Catering', 'post type singular name'),
+		'menu_name'             => _x( 'Catering', 'admin menu' ),
+		'name_admin_bar'        => _x( 'Catering', 'add new on admin bar' ),
+		'add_new'               => _x( 'Add New', 'catering' ),
+		'add_new_item'          => __( 'Add New Catering' ),
+		'new_item'              => __( 'New Catering' ),
+		'edit_item'             => __( 'Edit Catering' ),
+		'view_item'             => __( 'View Catering' ),
+		'all_items'             => __( 'All Catering' ),
+		'search_items'          => __( 'Search Catering' ),
+		'parent_item_colon'     => __( 'Parent Catering:' ),
+		'not_found'             => __( 'No catering found.' ),
+		'not_found_in_trash'    => __( 'No catering found in Trash.' ),
+		'archives'              => __( 'Catering Archives'),
+		'insert_into_item'      => __( 'Insert into catering'),
+		'uploaded_to_this_item' => __( 'Uploaded to this catering'),
+		'filter_item_list'      => __( 'Filter catering list'),
+		'items_list_navigation' => __( 'Catering list navigation'),
+		'items_list'            => __( 'Catering list'),
+		'featured_image'        => __( 'Catering featured image'),
+		'set_featured_image'    => __( 'Set catering featured image'),
+		'remove_featured_image' => __( 'Remove catering featured image'),
+		'use_featured_image'    => __( 'Use as featured image'),
+	);
+ 
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'show_in_nav_menus'  => true,
+		'show_in_admin_bar'  => true,
+		'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'catering' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'menu_icon'          => 'dashicons-archive',
+		'supports'           => array( 'title', 'editor' ),
+	);
+ 
+	register_post_type( 'cafe-catering', $args );
+
+	// Register Location CPT
+	$labels = array(
+		'name'                  => _x( 'Location', 'post type general name' ),
+		'singular_name'         => _x( 'Location', 'post type singular name'),
+		'menu_name'             => _x( 'Location', 'admin menu' ),
+		'name_admin_bar'        => _x( 'Location', 'add new on admin bar' ),
+		'add_new'               => _x( 'Add New', 'location' ),
+		'add_new_item'          => __( 'Add New Location' ),
+		'new_item'              => __( 'New Location' ),
+		'edit_item'             => __( 'Edit Location' ),
+		'view_item'             => __( 'View Location' ),
+		'all_items'             => __( 'All Locations' ),
+		'search_items'          => __( 'Search Location' ),
+		'parent_item_colon'     => __( 'Parent Location:' ),
+		'not_found'             => __( 'No location found.' ),
+		'not_found_in_trash'    => __( 'No locations found in Trash.' ),
+		'archives'              => __( 'Location Archives'),
+		'insert_into_item'      => __( 'Insert into location'),
+		'uploaded_to_this_item' => __( 'Uploaded to this location'),
+		'filter_item_list'      => __( 'Filter location list'),
+		'items_list_navigation' => __( 'Location list navigation'),
+		'items_list'            => __( 'Location list'),
+		'featured_image'        => __( 'Location featured image'),
+		'set_featured_image'    => __( 'Set location featured image'),
+		'remove_featured_image' => __( 'Remove location featured image'),
+		'use_featured_image'    => __( 'Use as featured image'),
+	);
+ 
+	$args = array(
+		'labels'             => $labels,
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'show_in_nav_menus'  => true,
+		'show_in_admin_bar'  => true,
+		'show_in_rest'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'location' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'menu_icon'          => 'dashicons-archive',
+		'supports'           => array( 'title', 'editor' ),
+	);
+ 
+	register_post_type( 'cafe-location', $args );
+
+};
+
+add_action( 'init', 'cafe_cheesecake_register_custom_post_types' );
+
+
+function cafe_cheesecake_register_taxonomies() {
+    // Add Menu Type taxonomy
+    $labels = array(
+        'name'              => _x( 'Menu Type', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Menu Type', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Menu Types' ),
+        'all_items'         => __( 'All Menu Type' ),
+        'parent_item'       => __( 'Parent Menu Type' ),
+        'parent_item_colon' => __( 'Parent Menu Type:' ),
+        'edit_item'         => __( 'Edit Menu Type' ),
+        'view_item'         => __( 'View Menu Type' ),
+        'update_item'       => __( 'Update Menu Type' ),
+        'add_new_item'      => __( 'Add New Menu Type' ),
+        'new_item_name'     => __( 'New Menu Type Name' ),
+        'menu_name'         => __( 'Menu Type' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'menu-type' ),
+    );
+    register_taxonomy( 'cafe-menu-type', array( 'cafe-menu' ), $args );
+
+
+    // Add Location Type taxonomy
+    $labels = array(
+        'name'                       => _x( 'Location Type', 'taxonomy general name' ),
+        'singular_name'              => _x( 'Location Type', 'taxonomy singular name' ),
+        'search_items'               => __( 'Location Type' ),
+        'all_items'                  => __( 'All Location Type' ),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __( 'Edit Location Type' ),
+        'update_item'                => __( 'Update Location Type' ),
+        'add_new_item'               => __( 'Add New Location Type' ),
+        'new_item_name'              => __( 'New Location Type Name' ),
+        'separate_items_with_commas' => __( 'Separate student type with commas' ),
+        'add_or_remove_items'        => __( 'Add or remove Location Types' ),
+        'choose_from_most_used'      => __( 'Choose from the most used student types' ),
+        'not_found'                  => __( 'No student types found.' ),
+        'menu_name'                  => __( 'Location Type' ),
+    );
+
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menu'      => true,
+        'show_in_rest'          => true,
+        'show_admin_column'     => true,
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'location-type' ),
+    );
+
+    register_taxonomy( 'cafe-location-type', array( 'cafe-jobs', 'cafe-location' ), $args );
+}
+add_action( 'init', 'cafe_cheesecake_register_taxonomies');
+
+
+// This flushes the permalinks if the theme is changed
+function cafe_cheesecake_rewrite_flush() {
+    cafe_cheesecake_register_custom_post_types();
+	cafe_cheesecake_register_taxonomies();
+    flush_rewrite_rules();
+}
+add_action( 'after_switch_theme', 'cafe_cheesecake_rewrite_flush' );
