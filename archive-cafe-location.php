c7a4dbab93bb98c2bccfcdec9dd3d68b86a69c7e
anhmaile <118566182+anhmaile@users.noreply.github.com>
2023-03-22 16:35:57 -0700

    Display Locations

--- archive-cafe-location.php
+++ archive-cafe-location.php
@@ -12,8 +12,6 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
@@ -22,30 +20,35 @@ get_header();
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				$args = array(
+					'post_type' => 'cafe-location',
+					'post_per_page' => -1,
+				);
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				$query = new WP_Query($args);
+				if($query -> have_posts()){
+					echo '<section class="location-container">';
+					while($query -> have_posts()){
+						$query -> the_post();
+	
+						if ( function_exists( 'get_field' ) ) {
+							?>
+							<h2><?php echo the_field('location_name'); ?></h2>
+							<p><?php echo the_field('location_information'); ?></p>	
+							<?php
+							echo wp_get_attachment_image( get_field( 'location_image' ), 'medium', '',
+								array(''));
+						}
+					}
+					wp_reset_postdata();
+					echo '</section>';
+				}
 
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+				echo '<section class="googlemaps-container">';
+				echo '</section>';
+			?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
