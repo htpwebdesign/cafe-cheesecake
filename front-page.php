98ad155ac22e4a4fc024fc8cca3a8a6e374f2b4f
brandonb95 <brandonbirk1995@gmail.com>
2023-03-22 17:43:30 -0700

    Cleaned up code at the beginning of homepage

--- front-page.php
+++ front-page.php
@@ -24,29 +24,8 @@ get_header();
 			get_template_part( 'template-parts/content', 'page' );
 			?>
 			<section class="home-blog">
-				<h2><?php esc_html_e( 'Latest News', 'rinhae' )?></h2><?php
-				$args = array(
-					'post_type'      => 'post',
-					'posts_per_page' => 3,
-				);
-				
-				$query = new WP_Query( $args );
-				
-				if ( $query->have_posts() ){
-					while ( $query->have_posts() ) {
-						$query->the_post(); 
-						?>
-
-						<article>
-							<a href="<?php the_permalink(); ?>">
-								<?php the_post_thumbnail(); ?>
-								<h3><?php the_title(); ?></h3>
-							</a>
-						</article>		
-						<?php			
-					}
-					wp_reset_postdata();
-				}
+				<?php
+			
 
 				if ( function_exists ( 'get_field' ) ) {
 					
