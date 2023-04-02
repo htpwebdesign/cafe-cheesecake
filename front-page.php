edbf9fd9f6db50471536ef26477b73c9b19bafbc
anhmaile <118566182+anhmaile@users.noreply.github.com>
2023-04-02 00:29:42 -0700

    Created @mixin full-grid-layout

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-		<section class="outer-container">
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -24,8 +24,6 @@ get_header();
 			?>
 			<section class="home-blog">
 				<?php
-			
-
 				if ( function_exists ( 'get_field' ) ) {
 					
 					// Repeater ACF Field
@@ -49,154 +47,159 @@ get_header();
 					}
 					?>
 
-					<section class="cheesecakes">
-					<article>
-					<?php
-					if ( get_field( 'our_cheesecakes_heading' ) ) {
-						echo '<h3>' . get_field( 'our_cheesecakes_heading' ) . '</h3>';
-					}
+					<section class="cheesecakes-container">
+						<section class="cheesecakes">
+							<article>
+							<?php
+							if ( get_field( 'our_cheesecakes_heading' ) ) {
+								echo '<h3>' . get_field( 'our_cheesecakes_heading' ) . '</h3>';
+							}
 
-					if ( get_field( 'our_cheesecakes_description' ) ) {
-						echo '<p>' . get_field( 'our_cheesecakes_description' ) . '</p>';
-					}
+							if ( get_field( 'our_cheesecakes_description' ) ) {
+								echo '<p>' . get_field( 'our_cheesecakes_description' ) . '</p>';
+							}
 
-					$url = get_field( 'our_cheesecakes_read_more' );
-						if ( get_field( 'our_cheesecakes_read_more' ) ) {
-							echo '<a href="' . $url . '" class="button">Read More</a>';
-						}
-					
-						?>
+							$url = get_field( 'our_cheesecakes_read_more' );
+								if ( get_field( 'our_cheesecakes_read_more' ) ) {
+									echo '<a href="' . $url . '" class="button">Read More</a>';
+								}
+							
+								?>
 
-					</article>
+							</article>
+						</section>
 					</section>
 
-					<section class="favourites">
-					<article>
-					<?php
-					if ( get_field( 'favourites_heading' ) ) {
-						echo '<h3>' . get_field( 'favourites_heading' ) . '</h3>';
-					}
-
-					// Relationship ACF Field
-
-					$relationship = get_field( 'favourites' );
-
-					if ( $relationship ) : ?>
-					<ul>
-						<?php foreach($relationship as $relation): ?>
-							<li>
-								<a href="<?php echo get_permalink( $relation->ID ); ?>">
-								<?php echo "<img src='" . get_the_post_thumbnail_url( $relation->ID ) . "'>" ?></a>
-								<?php echo get_the_title( $relation->ID ); ?>
-								<br>
-								<?php echo get_the_excerpt( $relation->ID ); ?>
-
-						</li>
-						<?php endforeach; ?>
-						</ul>
-						<?php endif; 
-
-					// See Menu Button
-					$urlfave = get_field( 'favourites_button' );
-					if ( get_field( 'favourites_button' ) ) {
-						echo '<a href="' . $urlfave . '" class="button">See Menu</a>';
-					}
-					?>
-				</article>
-				</section>
-
-				<section class="drinks">
-				<article>
-					<?php
-
-					if ( get_field( 'drinks_title' ) ) {
-						echo '<h3>' . get_field( 'drinks_title' ) . '</h3>';
-					}
-
-
-					
-					$parent_category_slug = 'drinks';
-					$parent_category_id = get_term_by('slug', $parent_category_slug, 'product_cat')->term_id;
-					$child_categories = get_term_children($parent_category_id, 'product_cat');
-
-					foreach ($child_categories as $child_category) {
-						$term = get_term_by('id', $child_category, 'product_cat');
-						$thumbnail_id = get_term_meta($child_category, 'thumbnail_id', true);
-
-						echo '<a href="' . get_term_link($term) . '">' . $term->name . '</a>';
-						echo '<a href="' . get_term_link($term) . '">' . wp_get_attachment_image($thumbnail_id, 'thumbnail') . '</a>';
-
-					}
-
-					// See Menu Button
-					$urldrinks = get_field( 'drinks_button' );
-					if ( get_field( 'drinks_button' ) ) {
-						echo '<a href="' . $urldrinks . '" class="button">See Menu</a>';
-					}
-
-					if ( get_field( 'merch_heading' ) ) {
-						echo '<h3>' . get_field( 'merch_heading' ) . '</h3>';
-					}
-
-					?>
-				</article>
-				</section>
-
-				<section class="merch">
-				<article>
-					<?php
-					// Display Merch Posts
-					$argsMerch = array(
-						'post_type' 		=> 'product',
-						'posts_per_page'	=> 5,
-						'oderby' 			=> 'term_id',
-						'tax_query' 		=> array(
-							array(
-								'taxonomy' 			=> 'product_cat',
-								'field' 			=> 'slug',
-								'terms' 			=> 'merch',
-								'include_children' 	=> false,
-							)
-							),
-					);
-					
-					$queryMerch = new WP_Query( $argsMerch );
-					
-					if ( $queryMerch->have_posts() ){
-						while ( $queryMerch->have_posts() ) {
-							$queryMerch->the_post(); 
-							global $product;
-							?>
-						
+					<section class="favourites-container">
+						<section class="favourites">
 							<article>
-								<a href="<?php the_permalink(); ?>">
-									<?php the_post_thumbnail(); ?>
-									<h3><?php the_title(); ?></h3>
-									<?php echo $product->get_price_html(); ?>
-								</a>
-							</article>		
-							<?php			
-						}
-						wp_reset_postdata();
-					}
+							<?php
+							if ( get_field( 'favourites_heading' ) ) {
+								echo '<h3>' . get_field( 'favourites_heading' ) . '</h3>';
+							}
+
+							// Relationship ACF Field
+
+							$relationship = get_field( 'favourites' );
+
+							if ( $relationship ) : ?>
+							<ul>
+								<?php foreach($relationship as $relation): ?>
+									<li>
+										<a href="<?php echo get_permalink( $relation->ID ); ?>">
+										<?php echo "<img src='" . get_the_post_thumbnail_url( $relation->ID ) . "'>" ?></a>
+										<?php echo get_the_title( $relation->ID ); ?>
+										<br>
+										<?php echo get_the_excerpt( $relation->ID ); ?>
+
+								</li>
+								<?php endforeach; ?>
+								</ul>
+								<?php endif; 
+
+							// See Menu Button
+							$urlfave = get_field( 'favourites_button' );
+							if ( get_field( 'favourites_button' ) ) {
+								echo '<a href="' . $urlfave . '" class="button">See Menu</a>';
+							}
+							?>
+						</article>
+					</section>
+				</section>
+
+				<section class="drinks-container">
+					<section class="drinks">
+						<article>
+							<?php
+
+							if ( get_field( 'drinks_title' ) ) {
+								echo '<h3>' . get_field( 'drinks_title' ) . '</h3>';
+							}
+
+
 							
-					
-					
-					$urlmerch = get_field( 'merch_section_button' );
-					if ( get_field( 'merch_section_button' ) ) {
-						echo '<a href="' . $urlmerch . '" class="button">Our Shop</a>';
-					}
+							$parent_category_slug = 'drinks';
+							$parent_category_id = get_term_by('slug', $parent_category_slug, 'product_cat')->term_id;
+							$child_categories = get_term_children($parent_category_id, 'product_cat');
 
-				  }
+							foreach ($child_categories as $child_category) {
+								$term = get_term_by('id', $child_category, 'product_cat');
+								$thumbnail_id = get_term_meta($child_category, 'thumbnail_id', true);
 
-				?>
-				</article>
+								echo '<a href="' . get_term_link($term) . '">' . $term->name . '</a>';
+								echo '<a href="' . get_term_link($term) . '">' . wp_get_attachment_image($thumbnail_id, 'thumbnail') . '</a>';
+
+							}
+
+							// See Menu Button
+							$urldrinks = get_field( 'drinks_button' );
+							if ( get_field( 'drinks_button' ) ) {
+								echo '<a href="' . $urldrinks . '" class="button">See Menu</a>';
+							}
+
+							if ( get_field( 'merch_heading' ) ) {
+								echo '<h3>' . get_field( 'merch_heading' ) . '</h3>';
+							}
+
+							?>
+						</article>
+					</section>
 				</section>
+
+				<section class="merch-container">
+					<section class="merch">
+						<article>
+							<?php
+							// Display Merch Posts
+							$argsMerch = array(
+								'post_type' 		=> 'product',
+								'posts_per_page'	=> 5,
+								'oderby' 			=> 'term_id',
+								'tax_query' 		=> array(
+									array(
+										'taxonomy' 			=> 'product_cat',
+										'field' 			=> 'slug',
+										'terms' 			=> 'merch',
+										'include_children' 	=> false,
+									)
+									),
+							);
+							
+							$queryMerch = new WP_Query( $argsMerch );
+							
+							if ( $queryMerch->have_posts() ){
+								while ( $queryMerch->have_posts() ) {
+									$queryMerch->the_post(); 
+									global $product;
+									?>
+								
+									<article>
+										<a href="<?php the_permalink(); ?>">
+											<?php the_post_thumbnail(); ?>
+											<h3><?php the_title(); ?></h3>
+											<?php echo $product->get_price_html(); ?>
+										</a>
+									</article>		
+									<?php			
+								}
+								wp_reset_postdata();
+							}
+									
+							$urlmerch = get_field( 'merch_section_button' );
+							if ( get_field( 'merch_section_button' ) ) {
+								echo '<a href="' . $urlmerch . '" class="button">Our Shop</a>';
+							}
+
+						}
+
+						?>
+						</article>
+					</section>
 				</section>
+			</section>
 		<?php
 		endwhile; // End of the loop.
 		?>
-		</section>
 	</main><!-- #main -->
 
 <?php
