3f34ed722bf114a6148aa58656cc0a1ae939743b
Alexander Sheh <alexgjsheh@gmail.com>
2023-04-02 23:22:38 -0700

    Removed sidebar from shop page

--- functions.php
+++ functions.php
@@ -101,6 +101,11 @@ function cafe_cheesecake_setup() {
 			'flex-height' => true,
 		)
 	);
+
+	// Remove sidebar from shop page
+
+	remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
+
 }
 add_action( 'after_setup_theme', 'cafe_cheesecake_setup' );
 
@@ -430,3 +435,7 @@ function cafe_send_new_post($new_status, $old_status, $post) {
   }
 
 add_action('transition_post_status', 'cafe_send_new_post', 10, 3);
+
+
+
+
